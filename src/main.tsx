10b58cc505ffb5dda3d7bea5b2f99c3961ab8506
Manish Jha <mjha205@rku.ac.in>
2025-01-29 01:35:52 +0530

    chore: added vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import App from './App.tsx';
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Providers>
       <App />
+      <SpeedInsights />
     </Providers>
   </StrictMode>
 );
