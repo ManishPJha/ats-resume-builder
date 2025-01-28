6e941a5bd93b0604115d3536852979abb032c698
Manish Jha <mjha205@rku.ac.in>
2025-01-29 01:31:09 +0530

    fix: minor fixes

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -7,10 +7,12 @@ const options: Auth0ProviderOptions = {
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
   authorizationParams: {
-    redirect_uri: import.meta.env.PROD
-      ? import.meta.env.VITE_AUTH0_REDIRECT_URL
-      : 'http://localhost:5173/',
+    redirect_uri: window.location.origin,
+    audience: 'https://dev-h0ojaj42rqgak4y1.us.auth0.com/api/v2/',
+    scope: 'openid profile email offline_access',
   },
+  useRefreshTokens: true,
+  cacheLocation: 'localstorage',
 };
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
